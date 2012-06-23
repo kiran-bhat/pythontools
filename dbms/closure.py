bc30cb2863e9dc57f35ee2c9b2730302efa0d72c
unknown <kiran@lenovos10.(none)>
2012-06-23 11:16:07 +0530

    Adding tools to find closure of functional dependencies

---
+++ dbms/closure.py
@@ -0,0 +1,49 @@
+import sys
+print "Program to find closure of an element in a given set of functional dependencies"
+print "Author: kiran19882004@gmail.com"
+print "Enter dependencies in following format"
+print "A->B,C;D->A;X,Y->Z"
+data = raw_input("Data: ")
+inputs=data.split(';')
+dictonary={}
+def issubset(a,b):
+    r=a.split(',')
+    for i in r:
+        if not i in b:
+            return False
+    return True
+def traverse(t):
+    px=[]
+    px.extend(t)
+    for k, v in dictonary.iteritems():
+        if issubset(k,t):
+            for d in v:
+                if not d in px:
+                    px.append(d)
+    return px
+for i in inputs:
+    if i !='':
+        p=[]
+        items=i.split('->')
+        if len(items)==2:
+            for x in items[1].split(','):
+                p.append(x)
+            if items[0]not in dictonary:
+                dictonary[items[0]]=p
+            else:
+                dictonary[items[0]].extend(p)
+        else:
+            raise NameError("Invalid input string")
+if(len(dictonary)==0):
+    raise NameError("Invalid input string")
+tracer=raw_input("Enter which dependencies closure you require")
+arr=[]
+if not tracer in dictonary:
+    print arr
+else:
+    arr.append(tracer)
+    newarr=traverse(arr)
+    while not sorted(newarr) == sorted(arr):
+        arr=newarr
+        newarr=traverse(arr)
+    print newarr
