f482b5e60617df803301dfe7454bb09797eecb00
Kiran Bhat <kiran19882004@gmail.com>
2012-06-23 17:07:00 +0530

    support for multi input with 2NF check

---
+++ dbms/dbfunctions.py
@@ -0,0 +1,70 @@
+class DBError(Exception):
+        def __init__(self, value):
+            self.value = value
+        def __str__(self):
+            return repr(self.value)
+def issubset(a,b):
+    r=a.split(',')
+    for i in r:
+        if not i in b:
+            return False
+    return True
+def traverse(t,dictonary):
+    px=[]
+    px.extend(t)
+    for k, v in dictonary.iteritems():
+        if issubset(k,t):
+            for d in v:
+                if not d in px:
+                    px.append(d)
+    return px
+def validateelements(ele,dictonary):
+    keyElements=ele.split(",")
+    for keyElement in keyElements:
+        if keyElement not in dictonary.iterkeys():
+            isInAnyDependency=False
+            for k, v in dictonary.iteritems():
+                if keyElement in v:
+                    isInAnyDependency=True
+                if keyElement in k.split(','):
+                    isInAnyDependency=True
+            if not isInAnyDependency:
+                raise DBError("ouch.. input values incorrect")
+    
+def find_closure(ele,dictonary):
+    validateelements(ele,dictonary)
+    arr=[]
+    for e in ele.split(','):
+        arr.append(e)
+        
+    newarr=traverse(arr,dictonary)
+    while not sorted(newarr) == sorted(arr):
+        arr=newarr
+        newarr=traverse(arr,dictonary)
+    return newarr
+
+def getAllElementsInFunctionalDependencies(fd):
+    elements=[]
+    for k, v in fd.iteritems():
+        if k not in elements:
+            for y in k.split(','):
+                if y not in elements:
+                    elements.append(y)
+        for x in v:
+            if x not in elements:
+                elements.append(x)
+    return elements
+    
+def checkKeyValidity(fd,key):
+    validateelements(key,fd)
+    if len(find_closure(key,fd))!=len(getAllElementsInFunctionalDependencies(fd)):
+        raise DBError("ouch.. key not valid")
+def isTwoNF(fd,key):
+    checkKeyValidity(fd,key)
+    if len(key.split(','))==1:
+        return True
+    else:
+        for keyElement in key.split(','):
+            if len(find_closure(keyElement,fd))>1:
+                return False
+        return True
--- dbms/normalforms.py
+++ dbms/normalforms.py
@@ -1,14 +1,11 @@
-import sys
 import dbfunctions
-
-print "Program to find closure of an element in a given set of functional dependencies"
+print "Program to find normal form of a database schema given all its functional dependencies"
 print "Author: kiran19882004@gmail.com"
 print "Enter dependencies in following format"
 print "A->B,C;D->A;X,Y->Z"
 data = raw_input("Data: ")
 inputs=data.split(';')
 dictonary={}
-
 for i in inputs:
     if i !='':
         p=[]
@@ -24,7 +21,8 @@ for i in inputs:
             raise NameError("Invalid input string")
 if(len(dictonary)==0):
     raise NameError("Invalid input string")
-tracer=raw_input("Enter which elements closure you require (use comma separation if you need combined closure of multiple elements)")
-
-
-print dbfunctions.find_closure(tracer,dictonary)
+tracer=raw_input("Enter which candidate key (use comma separation if you need a composite key)")
+if dbfunctions.isTwoNF(dictonary,tracer):
+    print "2NF!!!"
+else:
+    print "Not in 2NF"
